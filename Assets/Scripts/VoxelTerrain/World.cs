0431c329f003ea12c188ab78e0fda92826edd4f6
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-02-05 12:50:06 +0000

    Terrain generation using simplex noise

--- Assets/Scripts/VoxelTerrain/World.cs
+++ Assets/Scripts/VoxelTerrain/World.cs
@@ -18,11 +18,11 @@ namespace VoxelTerrain
         
         private void Start()
         {
-            for (int x = -2; x < 2; x++)
+            for (int x = -4; x < 4; x++)
             {
-                for (int y = -1; y < 1; y++)
+                for (int y = -1; y < 3; y++)
                 {
-                    for (int z = -1; z < 1; z++)
+                    for (int z = -4; z < 4; z++)
                     {
                         CreateChunk(x * 16, y * 16, z * 16);
                     }
@@ -66,27 +66,11 @@ namespace VoxelTerrain
             
             // Add it to the chunks dictionary with the position as the key
             chunks.Add(worldPos, newChunk);
-            
-            for (int xi = 0; xi < 16; xi++)
-            {
-                for (int yi = 0; yi < 16; yi++)
-                {
-                    for (int zi = 0; zi < 16; zi++)
-                    {
-                        if (yi <= 7)
-                        {
-                           SetBlock(x + xi, y + yi, z + zi, new BlockGrass()); 
-                        }
-                        else
-                        {
-                            SetBlock(x + xi, y + yi, z + zi, new BlockAir());
-                        }
-                    }   
-                }
-            }
-            
+
+            var terrainGen = new TerrainGen();
+            newChunk = terrainGen.ChunkGen(newChunk);
             newChunk.SetBlocksUnmodified();
-            Serialisation.Load(newChunk);
+            bool loaded = Serialisation.Load(newChunk);
         }
 
         public void DestroyChunk(int x, int y, int z)
