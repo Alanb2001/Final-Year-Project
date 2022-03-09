1fe13bce4c21bcd00b91030ad5f17cbecb97cf57
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-03-09 21:44:33 +0000

    Refactored some code and changed  the scene a bit

--- Assets/Scripts/VoxelTerrain/TerrainGen.cs
+++ Assets/Scripts/VoxelTerrain/TerrainGen.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using Noise.Hash;
 
 namespace VoxelTerrain
 {
@@ -11,15 +10,10 @@ namespace VoxelTerrain
         private float stoneMountainHeight = 48;
         private float stoneMountainFrequency = 0.008f;
         private float stoneMinHeight = -12;
-        //private float dirtBaseHeight = 1;
-        //private float dirtNoise = 0.04f;
-        //private float dirtNoiseHeight = 3;
         private float caveFrequency = 0.025f;
-        //private float treeFrequency = 0.2f;
-        
+
         private int caveSize = 20;
-        //private int treeDensity = 3;
-        
+
         public Chunk ChunkGen(Chunk chunk)
         {
             for (int x = chunk.pos.x - 3; x < chunk.pos.x + Chunk.chunkSize + 3; x++)
@@ -43,8 +37,6 @@ namespace VoxelTerrain
             }
 
             stoneHeight += GetNoise(x, 0, z, stoneBaseNoise, Mathf.FloorToInt(stoneBaseNoiseHeight));
-            //int dirtHeight = stoneHeight + Mathf.FloorToInt(dirtBaseHeight);
-            //dirtHeight += GetNoise(x, 100, z, dirtNoise, Mathf.FloorToInt(dirtNoiseHeight));
             for (int y = chunk.pos.y - 8; y < chunk.pos.y + Chunk.chunkSize; y++)
             {
                 // Get a value to base cave generation on
@@ -52,17 +44,6 @@ namespace VoxelTerrain
                 if (y <= stoneHeight && caveSize < caveChance)
                 {
                     SetBlock(x, y, z, new Block(), chunk);
-                }
-                else if (y <= /*dirtHeight*/ caveSize && caveSize < caveChance)
-                {
-                    //SetBlock(x, y, z, new BlockGrass(), chunk);
-
-                    SetBlock(x, y, z, new Block(), chunk);
-                    
-                    //if (y == dirtHeight && GetNoise(x, 0, z, treeFrequency, 100) < treeDensity)
-                    //{
-                    //    CreatTree(x, y + 1, z, chunk);
-                    //}
                 }
                 else
                 {
@@ -87,27 +68,6 @@ namespace VoxelTerrain
             }
         }
 
-        void CreatTree(int x, int y, int z, Chunk chunk)
-        {
-            // Create leaves
-            for (int xi = -2; xi <= 2; xi++)
-            {
-                for (int yi = 4; yi <= 8; yi++)
-                {
-                    for (int zi = -2; zi <= 2; zi++)
-                    {
-                        SetBlock(x + xi, y + yi, z + zi, new BlockLeaves(), chunk, true);
-                    }
-                }
-            }
-            
-            // Create trunk
-            for (int yt = 0; yt < 6; yt++)
-            {
-                SetBlock(x, y + yt, z, new BlockWood(), chunk, true);
-            }
-        }
-        
         public static int GetNoise(int x, int y, int z, float scale, int max)
         {
             return Mathf.FloorToInt((Noise.Generate(x * scale, y * scale, z * scale) + 1f) * (max / 2f));
