d35621ddcaeb332947469ebd34e543696d9f6a34
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-03-16 22:34:19 +0000

    Looks more like a cave and now procedurally generates in the x y z axis

--- Assets/Scripts/VoxelTerrain/TerrainGen.cs
+++ Assets/Scripts/VoxelTerrain/TerrainGen.cs
@@ -4,14 +4,8 @@ namespace VoxelTerrain
 {
     public class TerrainGen
     {
-        private float stoneBaseHeight = -24;
-        private float stoneBaseNoise = 0.05f;
-        private float stoneBaseNoiseHeight = 4;
-        private float stoneMountainHeight = 48;
-        private float stoneMountainFrequency = 0.008f;
-        private float stoneMinHeight = -12;
         private float caveFrequency = 0.025f;
-
+        
         private int caveSize = 20;
 
         public Chunk ChunkGen(Chunk chunk)
@@ -28,30 +22,22 @@ namespace VoxelTerrain
         }
 
         public Chunk ChunkColumnGen(Chunk chunk, int x, int z)
-        {
-            int stoneHeight = Mathf.FloorToInt(stoneBaseHeight);
-            stoneHeight += GetNoise(x, 0, z, stoneMountainFrequency, Mathf.FloorToInt(stoneMountainHeight));
-            if (stoneHeight < stoneMinHeight)
-            {
-                stoneHeight = Mathf.FloorToInt(stoneMinHeight);
-            }
-
-            stoneHeight += GetNoise(x, 0, z, stoneBaseNoise, Mathf.FloorToInt(stoneBaseNoiseHeight));
-            for (int y = chunk.pos.y - 8; y < chunk.pos.y + Chunk.chunkSize; y++)
-            {
-                // Get a value to base cave generation on
-                int caveChance = GetNoise(x, y, z, caveFrequency, 100);
-                if (y <= stoneHeight && caveSize < caveChance)
-                {
-                    SetBlock(x, y, z, new Block(), chunk);
-                }
-                else
-                {
-                    SetBlock(x, y, z, new BlockAir(), chunk);
-                }
-            }
+        { 
+           for (int y = chunk.pos.y - 8; y < chunk.pos.y + Chunk.chunkSize; y++)
+           {
+               // Get a value to base cave generation on
+               int caveChance = GetNoise(x, y, z, caveFrequency, 60);
+               if (caveSize < caveChance)
+               {
+                   SetBlock(x, y, z, new Block(), chunk);
+               }
+               else
+               {
+                   SetBlock(x, y, z, new BlockAir(), chunk);
+               }
+           }
             
-            return chunk;
+           return chunk;
         }
 
         public static void SetBlock(int x, int y, int z, Block block, Chunk chunk, bool replaceBlocks = false)
